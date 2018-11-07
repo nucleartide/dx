77efe0c0a5d6bec652692c05a34ddff156c69d5c
Jason Tu <jasontu4@gmail.com>
2018-11-06 23:54:48 -0800

    Remove tabs

--- example.js
+++ example.js
@@ -7,13 +7,13 @@ async function sleepThenFail(ms) {
 }
 
 ;(async function main() {
-	let [res, err] = dx(JSON.parse)('fail')
-	if (err) {
-		console.error(err.stack)
-	}
+  let [res, err] = dx(JSON.parse)('fail')
+  if (err) {
+    console.error(err.stack)
+  }
 
-	[res, err] = await dx(sleepThenFail)(1000)
-	if (err) {
-		console.error(err.stack)
-	}
+  [res, err] = await dx(sleepThenFail)(1000)
+  if (err) {
+    console.error(err.stack)
+  }
 })()
