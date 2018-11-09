2e097526c7108c3aa2ccd0b0991bd56daec82bfc
Jason Tu <jasontu4@gmail.com>
2018-11-09 08:16:57 -0800

    Add bound method example

--- example.js
+++ example.js
@@ -6,14 +6,31 @@ async function sleepThenFail(ms) {
   throw new Error('fail')
 }
 
+class Dog {
+  constructor() {
+    this.species = 'shiba inu'
+  }
+
+  async eatFood() {
+    if (!this || !this.species) throw new Error('Forgot to bind method.')
+    await sleepThenFail(1000)
+  }
+}
+
 ;(async function main() {
-  let [res, err] = dx(JSON.parse)('fail')
+  const [res, err] = dx(JSON.parse)('fail')
   if (err) {
     console.error(err.stack)
   }
 
-  [res, err] = await dx(sleepThenFail)(1000)
-  if (err) {
-    console.error(err.stack)
+  const [res2, err2] = await dx(sleepThenFail)(1000)
+  if (err2) {
+    console.error(err2.stack)
+  }
+
+  const d = new Dog()
+  const [res3, err3] = await dx(d.eatFood, d)()
+  if (err3) {
+    console.error(err3.stack)
   }
 })()
